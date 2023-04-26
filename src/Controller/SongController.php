8470c46d7b2c74a2e36a67a5516e08a459932507
Johannes Nielsen <jn@koehler-partner.de>
2023-04-26 16:00:01 +0200

    add logger service to SongController

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class SongController extends AbstractController
 {
     #[Route('/api/songs/{id<\d+>}', name: 'app_song', methods: ['GET'])]
-    public function getSongAction(int $id): Response
+    public function getSongAction(int $id, LoggerInterface $logger): Response
     {
         // TODO query the database
         $song = [
@@ -17,6 +18,10 @@ class SongController extends AbstractController
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info('Returning API response for song {song}', [
+            'song' => $id,
+        ]);
 
         return $this->json($song);
     }
